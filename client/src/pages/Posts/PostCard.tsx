33d0ba7eb446f5de5e83aad0ba6d0db2b875dfe8
Nic Fan <nicfan@gmail.com>
2023-02-28 15:50:59 -0800

    Updated layout of posts in the homepage

--- client/src/pages/Posts/PostCard.tsx
+++ client/src/pages/Posts/PostCard.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { FiThumbsUp, FiMessageCircle } from 'react-icons/fi';
+import { FiMapPin, FiThumbsUp, FiMessageCircle } from 'react-icons/fi';
 import dayjs from 'dayjs';
 
 interface IPostCardProps {
@@ -9,21 +9,27 @@ interface IPostCardProps {
 const PostCard = ({ post }: IPostCardProps): React.ReactElement => {
 	const navigate = useNavigate();
 
-	const { id, imgUrl, caption, likes, comments, createdAt } = post;
+	const { id, imgUrl, location, caption, likes, comments, createdAt } = post;
 
 	return (
 		<div
 			className="w-full sm:w-5/12 md:w-auto flex flex-col bg-white hover:-translate-y-2 hover:cursor-pointer transition-all"
 			onClick={() => navigate(`/posts/${id}`)}
 		>
-			<img src={imgUrl} alt={id} className="h-60 md:h-36 w-full object-cover" />
+			<img src={imgUrl} alt={imgUrl} className="h-60 md:h-36 w-full object-cover" />
 
-			<div className="py-2 px-4">
-				<div className="flex justify-end items-center">
-					<p className="text-slate-400 italic">{dayjs(createdAt).format('DD/MM/YYYY h:mm a')}</p>
+			<div className="mb-2 py-2 px-4">
+				<div className="flex flex-col text-slate-500 text-base md:text-sm">
+					{location && (
+						<p className="flex gap-2 items-center">
+							<FiMapPin />
+							{location}
+						</p>
+					)}
+					<p className="italic">{dayjs(createdAt).format('DD/MM/YYYY h:mm a')}</p>
 				</div>
 
-				<div className="mb-3 text-lg">{caption}</div>
+				<p className="mb-3 text-xl md:text-lg">{caption}</p>
 
 				<div className="flex gap-4 items-center mb-2">
 					<div className="flex items-center text-2xl">
